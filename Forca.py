a0c591e4d6cfe4c0642a70415a1554f44f9b6d35
Dangerred <msfreire52@gmail.com>
2022-10-24 20:46:26 -0300

    Update Forca.py

--- Forca.py
+++ Forca.py
@@ -4,12 +4,21 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "chave"
+    palavra_secreta = "banana"
     enforcado = False
     jogando = False
+    lista = ["_", "_", "_", "_", "_", "_"]
 
     while not enforcado and not jogando:
-        print("Jogando...")
+        chute = input("Qual letra você deseja? ").lower().strip()
+        posicao = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                lista[posicao] = chute
+            posicao += 1
+        for i in lista:
+            print(i, end=" ")
+        print("\nJogando...")
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
