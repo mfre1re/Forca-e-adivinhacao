8acf8f188e9bbe58554a74ca9e136490179b51a4
Dangerred <msfreire52@gmail.com>
2022-10-25 21:22:51 -0300

    Update Forca.py

--- Forca.py
+++ Forca.py
@@ -6,23 +6,28 @@ def jogar():
 
     palavra_secreta = "banana"
     chances = 0
+    enforcado = False
     lista = ["_", "_", "_", "_", "_", "_"]
 
-    while chances != 6:
+    while enforcado == False:
         chute = input("Qual letra você deseja? ").lower().strip()
         posicao = 0
+
         for letra in palavra_secreta:
             if chute == letra:
                 lista[posicao] = chute
             posicao += 1
-        if chute not in "banana":
+        if chances == 5 and chute not in palavra_secreta:
+            break
+        elif chute not in palavra_secreta:
             print(f"Menos uma chance! Restam apenas {5 - chances}!")
             chances += 1
         for i in lista:
             print(i, end=" ")
 
+        enforcado = "_" not in lista
         print("\nJogando...")
-    print("Fim de jogo!")
+    print("\nFim de jogo!")
 
 if __name__ == "__main__":
     jogar()
